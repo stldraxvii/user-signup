06ac7cc56309c337b188f10a3fb257ecbd9561b5
Matt Kemper <matt.kemper90@gmail.com>
2017-03-30 18:00:54 -0500

    added non-functional boxes with labels

--- main.py
+++ main.py
@@ -16,9 +16,45 @@
 #
 import webapp2
 
+def build_page(username='', password='', confirm='', email=''):
+    head = "<h2>" + "User Signup Form" + "</h2>"
+
+    name_label = "<label>Username</label>"
+    name_area = "<input type='text' name='username'>" + username + "</input>"
+    pass_label = "<label>Password</label>"
+    pass_area = "<input type='password' name='password'>" + password + "</input>"
+    confirm_label = "<label>Confirm Password</label>"
+    confirm_area = "<input type='password' name='confirm'>" + confirm + "</input>"
+    email_label = "<label>Email (Optional)</label>"
+    email_area = "<input type='text' name='email'>" + email + "</input>"
+    submit = "<input type='submit'/>"
+
+    form = ("<form method='post'>" + "<table>" +
+        "<tr>" + "<td>" + name_label + "</td>" + "<td>" + name_area + "</td>" + "</tr>" +
+        "<tr>" + "<td>" + pass_label + "</td>" + "<td>" + pass_area + "</td>" + "</tr>" +
+        "<tr>" + "<td>" + confirm_label + "</td>" + "<td>" + confirm_area + "</td>" + "</tr>" +
+        "<tr>" + "<td>" + email_label + "</td>" + "<td>" + email_area + "</td>" + "</tr>" +
+        "<tr>" + "<td>" + submit + "</td>" + "</tr>" +
+        "</table>" +
+        "</form>")
+
+    return head + form
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        content = build_page('')
+
+        self.response.write(content)
+
+    def post(self):
+        username = self.request.get("username")
+        password = self.request.get("password")
+        confirm = self.request.get("confirm")
+        email = self.request.get("email")
+
+        content = build_page(username, password, confirm, email)
+
+        self.response.write(content)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
